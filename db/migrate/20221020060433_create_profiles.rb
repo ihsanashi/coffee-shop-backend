869e6de050b0f8a64626e6210697506052a6a9a3
Ahmad Ihsan <ihsanashi@gmail.com>
2022-10-20 14:19:18 +0800

    feat: generate profile model

---
+++ db/migrate/20221020060433_create_profiles.rb
@@ -0,0 +1,14 @@
+class CreateProfiles < ActiveRecord::Migration[7.0]
+  def change
+    create_table :profiles do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :hashed_password
+      t.string :phone
+
+      t.timestamps
+    end
+  end
+end
