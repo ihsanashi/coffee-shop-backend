07de7c4c82ff211c94dd47e4fa026886404f0252
Ahmad Ihsan <ihsanashi@gmail.com>
2022-10-20 14:19:01 +0800

    feat: generate user model

---
+++ db/migrate/20221020060317_create_users.rb
@@ -0,0 +1,10 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.references :role, null: false, foreign_key: true
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
