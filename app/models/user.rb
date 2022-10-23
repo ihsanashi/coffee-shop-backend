8e339cb875163f3b4182b597a4450ad66d949861
Ahmad Ihsan <ihsanashi@gmail.com>
2022-10-23 12:47:01 +0800

    fix: email field validation format

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_one :profile
 
   has_secure_password
-  validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 80 }, format: { with: ConstantData::VALID_EMAIL_REGEX }
-  validates :password_digest, presence :true, length: { minimum: 6 }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 80 }, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password_digest, presence: true, length: { minimum: 6 }
 
 end
