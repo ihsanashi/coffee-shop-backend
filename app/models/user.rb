1d578fca3484ca76d5656b4185ebf625ca3212d8
Ahmad Ihsan <ihsanashi@gmail.com>
2022-10-23 09:02:13 +0800

    fix: add validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
   belongs_to :role
   has_one :profile
+
+  has_secure_password
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 80 }, format: { with: ConstantData::VALID_EMAIL_REGEX }
+  validates :password_digest, presence :true, length: { minimum: 6 }
+
 end
