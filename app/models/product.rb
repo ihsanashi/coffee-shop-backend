2a3c77c742e03ae82269664f9dbe161a50721b7f
Ahmad Ihsan <ihsanashi@gmail.com>
2022-10-23 12:45:03 +0800

    fix: allow products to have many tags

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,8 @@
 class Product < ApplicationRecord
   has_many :product_images
   has_many :product_variants
+  has_many :product_tags
   has_one :product_category
   has_one :product_note
-  has_one :product_tag
   has_one :product_tax
 end
