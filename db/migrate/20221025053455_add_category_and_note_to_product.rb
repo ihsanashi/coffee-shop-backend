e0641ae77433ac7def30e245c9d63f0d816dceb3
Ahmad Ihsan <ihsanashi@gmail.com>
2022-10-25 13:37:13 +0800

    fix: add migration for adding category and note columns to product

---
+++ db/migrate/20221025053455_add_category_and_note_to_product.rb
@@ -0,0 +1,6 @@
+class AddCategoryAndNoteToProduct < ActiveRecord::Migration[7.0]
+  def change
+    add_column :products, :category, :string
+    add_column :products, :note, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_25_050358) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_25_053455) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -141,6 +141,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_25_050358) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slug"
+    t.string "category"
+    t.string "note"
   end
 
   create_table "profiles", force: :cascade do |t|
